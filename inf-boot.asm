d3759319516c46848f31759f6b9d1fa9fc8be422
EntropicProblems <155194337+EntropicProblems@users.noreply.github.com>
2026-03-11 09:21:20 -0400

    Delete inf-boot.asm

--- inf-boot.asm
+++
@@ -1,8 +0,0 @@
-;Infinite Loop that makes our boot continue forever
-loop:
-	jmp loop
-;Now to make it 512 bytes, to make it a perfect sector size
-times 510-($-$$) db 0
-;$ is used to show the current position, $$ is used to show the starting position of the current section
-;now for the magic number, that specifies this sector as a boot sector
-dw 0xaa55 ;thats the number
